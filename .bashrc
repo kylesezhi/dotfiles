9128ef32d24bba0796f570c2693f2c97434bdfc5
Kyle Bedell <kyle.bedell@gmail.com>
2019-06-01 09:38:45 -0400

    Improve setup.sh and bashrc

--- .bashrc
+++ .bashrc
@@ -6,14 +6,8 @@ set -o vi
 
 # Find and replace text in multiple files,
 # honor gitignore.
-# Usage: kae oldPattern newPattern
-kae ()
-{
-  git grep --name-only $1 | xargs sed -i "s/$1/$2/"
-}
-
-alias ~='cd ~'
-alias gst='git status'
+# Usage: kae oldPattern newText
+kae () { git grep --name-only $1 | xargs sed -i "s/$1/$2/" }
 
 # Change Prompt
 export PS1="____________________________________________________________\n| \[\033[1;34m\]\!\[\033[0m\] \[\033[1;35m\]\u\[\033[0m\] @ \[\033[1;35m\]\w\[\033[0m\]\n| 🐡 💨  "
@@ -26,3 +20,6 @@ alias mkdir='mkdir -pv'                     # Preferred 'mkdir' implementation
 alias ll='ls -FGlAhp'                       # Preferred 'ls' implementation
 alias less='less -FSRXc'                    # Preferred 'less' implementation
 cd() { builtin cd "$@"; ll; }               # Always list directory contents upon 'cd'
+alias ~='cd ~'
+alias gst='git status'
+mcd () { mkdir -p "$1" && cd "$1"; }        # mcd:          Makes new Dir and jumps inside
--- setup.sh
+++ setup.sh
@@ -1,25 +1,42 @@
 #!/usr/bin/env bash
 
+# If any command fails, abort this script
+set -e
+
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 VIM="/.vimrc"
 TMUX="/.tmux.conf"
 
-echo "Making symlinks to .vimrc and .tmux.conf"
-# ln -s $THISDIR$VIM ~/$VIM
-# ln -s $THISDIR$TMUX ~/$TMUX
+echo "Making symlinks to .vimrc"
+ln -s $THISDIR$VIM ~/$VIM
 
 echo "Installing vim-plug"
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Installing vim plugins"
+vim +'PlugInstall --sync' +qa
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     echo "Installing Mac OS software"
     brew install ag
+
+    # Need these for You Complete Me in vim
+    brew install cmake macvim
+
+    echo "Making symlinks to .tmux.conf"
+    ln -s $THISDIR$TMUX ~/$TMUX
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     echo "Installing Linux software"
     sudo apt-get install silversearcher-ag -y
+
+    # Need these for You Complete Me in vim
+    sudo apt install build-essential cmake3 python3-dev -y
+
+    echo "Making symlinks to .tmux.conf"
+    ln -s $THISDIR$TMUX ~/$TMUX
 elif [[ "$OSTYPE" == "msys" ]]; then
     echo "Installing Windows software"
 fi
 
-echo "==> Now run :PlugInstall in vim!"
+sudo ~/.vim/plugplugplug/youcompleteme/install.py --all
