5e853db02fcbc679f5369a526728c7fc6d4dfcb4
Kyle Bedell <kyle.bedell@gmail.com>
2019-07-04 19:31:47 -0400

    Update mac stuff

--- mac_setup.sh
+++ mac_setup.sh
@@ -8,12 +8,14 @@ VIM="/.vimrc"
 BASH="/.bashrc"
 TMUX="/.tmux.conf"
 
+################################################################################
 echo "Making symlinks to .vimrc, .tmux.conf and .bash_profile"
 ln -s $THISDIR$VIM ~/$VIM
 ln -s $THISDIR$TMUX ~/$TMUX
 ln -s $THISDIR$BASH ~/$BASH
 ln -s ~/.bashrc ~/.bash_profile
 
+################################################################################
 echo "Installing vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -21,12 +23,21 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 echo "Installing vim plugins"
 vim +'PlugInstall --sync' +qa
 
+################################################################################
 # echo "Installing Fira Code nerd font"
 # ~/.vim/plugplugplug/nerd-fonts/install.sh FiraCode
 
-echo "Installing Mac OS software"
-brew install ag
+################################################################################
+echo "Installing brews 🍺"
+if [ -e $cwd/swag/brews ]; then
+	chapter "Installing Homebrew formulae…"
 
-# You Complete Me in vim
-brew install cmake macvim
+	for brew in $(<$cwd/swag/brews); do
+		install_brews $brew
+	done
+fi
+
+################################################################################
+echo "Installing You Complete Me"
 sudo ~/.vim/plugplugplug/youcompleteme/install.py --all
+
